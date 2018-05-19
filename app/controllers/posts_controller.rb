569ed59de6422fab164b0bd3559b878e74ddffa1
Yorkego <Yorkego1@ukr.net>
2018-05-19 13:52:36 +0300

    Add flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,10 +30,10 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to @post, success: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
-        format.html { render :new }
+        format.html { render :new, danger: 'Post was not created.'  }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
@@ -44,10 +44,10 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.html { redirect_to @post, success: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
-        format.html { render :edit }
+        format.html { render :edit,  danger: 'Post was not updated.' }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
@@ -58,7 +58,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.html { redirect_to posts_url, success: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
