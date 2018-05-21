6cb569df756645c37ebf8c33b45c8dd8ee813998
Yorkego <Yorkego1@ukr.net>
2018-05-21 12:42:00 +0300

    Add tags cloud

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 	  root "posts#index"
 
 	  devise_for :users
-	  resources :posts	  
+	  resources :posts
+	  resources :tags, only: [:show]	  
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
