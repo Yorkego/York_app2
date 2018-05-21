479e8c5780547cc04221a8fef7b6a5fc157d84cf
Yorkego <Yorkego1@ukr.net>
2018-05-21 12:07:09 +0300

    Add tas to model and post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,7 @@ class PostsController < ApplicationController
 
   # GET /posts/1
   # GET /posts/1.json
-  def show
-    @post = Post.find(params[:id])
+  def show  
   end
 
   # GET /posts/new
@@ -71,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content, :image)
+      params.require(:post).permit(:title, :content, :image, :all_tags)
     end
 end
