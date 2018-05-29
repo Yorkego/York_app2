be90f2072847e9dcea51e92411895d232ffa9d5e
Yorkego <Yorkego1@ukr.net>
2018-05-29 16:14:22 +0300

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-   config.action_mailer.default_url_options = { host: 'thawing-crag-82626.herokuapp.com' }
+   config.action_mailer.default_url_options = { host: 'ancient-oasis-91737.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
