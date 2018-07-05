0d9f48d7edefde9af13a2b6d1db71c1f77c8ba0b
Yorkego <Yorkego1@ukr.net>
2018-07-05 14:25:59 +0300

    Fix some validation in frindship

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def update
   end
 
-   private
+  private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -4,17 +4,12 @@ class Friendship < ApplicationRecord
 
   validates :user_id, uniqueness: { scope: :friend_id, message: "you already send him request" }
   validate :users_are_not_already_friends
-  validate :not_self
 
   private
 
-  def not_self
-    errors.add(:friend, "can't befriend yourself") if user == friend
-  end
-
   def users_are_not_already_friends
-    combinations = ["user_id = #{user_id} AND friend_id = #{friend_id}",
-    "user_id = #{friend_id} AND friend_id = #{user_id}"]
+    combinations = ["user_id = #{user_id} AND friend_id = #{friend_id} AND accepted = #{accepted}",
+    "user_id = #{friend_id} AND friend_id = #{user_id} AND accepted = #{accepted}"]
     if Friendship.where(combinations.join(' OR ')).exists?
       self.errors.add(:user_id, 'Already friends!')
     end
